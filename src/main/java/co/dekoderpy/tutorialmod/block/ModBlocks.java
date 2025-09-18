c6d676019fae03ac189bcb00abfd57a67fb80c94
Dekoder-py <dekod3r.py@gmail.com>
2025-09-19 07:00:07 +1200

    Add ore blocks and refactor register block

--- src/main/java/co/dekoderpy/tutorialmod/block/ModBlocks.java
+++ src/main/java/co/dekoderpy/tutorialmod/block/ModBlocks.java
@@ -4,6 +4,7 @@ import co.dekoderpy.tutorialmod.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -13,17 +14,22 @@ import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
-    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block", AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK));
-    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block", AbstractBlock.Settings.create().strength(3f).requiresTool());
+    public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block", new Block(AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.AMETHYST_BLOCK).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, "pink_garnet_block")))));
+    public static final Block RAW_PINK_GARNET_BLOCK = registerBlock("raw_pink_garnet_block", new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, "raw_pink_garnet_block")))));
 
-    public static final Block BLUE_GEMSTONE_BLOCK = registerBlock("blue_gemstone_block", AbstractBlock.Settings.create().strength(3f).requiresTool());
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore", new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.create().strength(3f).requiresTool().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, "pink_garnet_ore")))));
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore", new ExperienceDroppingBlock(UniformIntProvider.create(2, 5), AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE).registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, "pink_garnet_deepslate_ore")))));
 
 
-    private static Block registerBlock(String name, AbstractBlock.Settings blockSettings) {
+    public static final Block BLUE_GEMSTONE_BLOCK = registerBlock("blue_gemstone_block", new Block(AbstractBlock.Settings.create().strength(3f).requiresTool().registryKey(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, "blue_gemstone_block")))));
+
+
+    private static Block registerBlock(String name, Block block) {
         RegistryKey<Block> key = RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(TutorialMod.MOD_ID, name));
-        Block block = new Block(blockSettings.registryKey(key));
+
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, key, block);
 
@@ -42,6 +48,10 @@ public class ModBlocks {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.BUILDING_BLOCKS).register(entries -> {
             entries.add(PINK_GARNET_BLOCK);
             entries.add(RAW_PINK_GARNET_BLOCK);
+
+            entries.add(PINK_GARNET_ORE);
+            entries.add(PINK_GARNET_DEEPSLATE_ORE);
+
             entries.add(BLUE_GEMSTONE_BLOCK);
         });
     }
