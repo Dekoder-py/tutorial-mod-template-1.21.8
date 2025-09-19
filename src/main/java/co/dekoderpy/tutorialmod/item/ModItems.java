036d73ec214e690040819b06b15f69ab64943b17
Dekoder-py <dekod3r.py@gmail.com>
2025-09-20 07:06:13 +1200

    Create a chisel item

--- src/main/java/co/dekoderpy/tutorialmod/item/ModItems.java
+++ src/main/java/co/dekoderpy/tutorialmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package co.dekoderpy.tutorialmod.item;
 
 import co.dekoderpy.tutorialmod.TutorialMod;
+import co.dekoderpy.tutorialmod.item.custom.ChiselItem;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -13,6 +14,18 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item PINK_GARNET = registerItem("pink_garnet", new Item.Settings());
     public static final Item RAW_PINK_GARNET = registerItem("raw_pink_garnet", new Item.Settings());
+
+    public static final ChiselItem CHISEL = registerChiselItem("chisel", new Item.Settings().maxDamage(32));
+
+    private static ChiselItem registerChiselItem(String name, Item.Settings itemSettings) {
+
+        RegistryKey<Item> key = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TutorialMod.MOD_ID, name));
+
+        ChiselItem item = new ChiselItem(itemSettings.registryKey(key));
+
+        return Registry.register(Registries.ITEM, key, item);
+
+    }
 
     private static Item registerItem(String name, Item.Settings itemSettings) {
 
@@ -30,6 +43,10 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PINK_GARNET);
             entries.add(RAW_PINK_GARNET);
+        });
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> {
+            entries.add(CHISEL);
         });
     }
 }
