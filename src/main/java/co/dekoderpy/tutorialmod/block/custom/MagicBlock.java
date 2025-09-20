99a4d160b0c4b69778030ff2468b35993eafccc6
Dekoder-py <dekod3r.py@gmail.com>
2025-09-20 16:20:50 +1200

    Magic block nerf and buff

--- src/main/java/co/dekoderpy/tutorialmod/block/custom/MagicBlock.java
+++ src/main/java/co/dekoderpy/tutorialmod/block/custom/MagicBlock.java
@@ -1,5 +1,6 @@
 package co.dekoderpy.tutorialmod.block.custom;
 
+import co.dekoderpy.tutorialmod.block.ModBlocks;
 import co.dekoderpy.tutorialmod.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -23,7 +24,7 @@ public class MagicBlock extends Block {
 
     @Override
     protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        world.playSound(player, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 1f, 1f);
+        world.playSound(player, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_CHIME, SoundCategory.BLOCKS, 2.5f, 1f);
         return ActionResult.SUCCESS;
     }
 
@@ -32,10 +33,17 @@ public class MagicBlock extends Block {
         if (entity instanceof ItemEntity itemEntity) {
             if (itemEntity.getStack().getItem() == ModItems.RAW_PINK_GARNET) {
                 itemEntity.setStack(new ItemStack(Items.DIAMOND, itemEntity.getStack().getCount()));
-                world.playSound(entity, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_RESONATE, SoundCategory.BLOCKS, 1f, 1f);
+                world.playSound(entity, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_RESONATE, SoundCategory.BLOCKS, 2f, 1f);
+                world.setBlockState(pos, ModBlocks.PINK_GARNET_BLOCK.getDefaultState());
+
 
             }
+        } else if (entity instanceof PlayerEntity playerEntity) {
+            playerEntity.giveItemStack(new ItemStack(ModItems.CHISEL, 1));
+            world.playSound(entity, pos, SoundEvents.BLOCK_AMETHYST_BLOCK_RESONATE, SoundCategory.BLOCKS, 2f, 1f);
+            world.setBlockState(pos, ModBlocks.PINK_GARNET_BLOCK.getDefaultState());
         }
+
         super.onSteppedOn(world, pos, state, entity);
     }
 }
